5e4ced57c755162ebfc683b28219749c87463fc6
Ростислав <kingartes1989@gmail.com>
2022-02-16 16:19:03 +0200

    Add fire action, refactor weapon component

---
+++ Source/ShootThemUp/Private/Components/STUWeaponComponent.cpp
@@ -0,0 +1,42 @@
+// Shoot them up Game, All right Reserved
+
+
+#include "Components/STUWeaponComponent.h"
+#include "Weapon/STUBaseWeapon.h"
+#include "GameFramework/Character.h"
+
+USTUWeaponComponent::USTUWeaponComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+}
+
+
+// Called when the game starts
+void USTUWeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpawnWeapon();
+}
+
+void USTUWeaponComponent::SpawnWeapon() {
+	if (!GetWorld()) return;
+
+	ACharacter* Character = Cast<ACharacter>(GetOwner());
+	if (!Character) return;
+
+	CurrentWeapon = GetWorld()->SpawnActor<ASTUBaseWeapon>(WeaponClass);
+	if (!CurrentWeapon) return;
+
+	FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, false);
+	CurrentWeapon->AttachToComponent(Character->GetMesh(), AttachmentRules, WeaponAttachPointName);
+
+}
+
+void USTUWeaponComponent::Fire() {
+	if (!CurrentWeapon) return;
+
+	CurrentWeapon->Fire();
+}
+
