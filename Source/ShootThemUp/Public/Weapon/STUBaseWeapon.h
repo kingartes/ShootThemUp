e81fd90120871b8ef3f10bf1db6785697216e102
Ростислав <kingartes1989@gmail.com>
2022-02-17 16:04:05 +0200

    Makeshot function refactor

--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -31,4 +31,11 @@ protected:
 	virtual void BeginPlay() override;
 
 	void MakeShot();
+
+private:
+	APlayerController* GetPlayerController() const;
+	bool GetPlayerViewPoint(FVector& ViewLocation, FRotator& ViewRotation) const;
+	FVector GetMuzzleWorldLocation() const;
+	bool GetTraceData(FVector& TraceStart, FVector& TraceEnd) const;
+	void MakeHit(FHitResult& HitResult, const FVector& TraceStart, const FVector& TraceEnd);
 };
