44ebb02269d895df9024e7143ca5b0f5a6032980
Ростислав <kingartes1989@gmail.com>
2022-02-17 15:31:13 +0200

    Add line trace shooting for rifle

--- Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
@@ -3,6 +3,10 @@
 
 #include "Weapon/STUBaseWeapon.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "GameFrameWork/Character.h"
+#include "GameFrameWork/Controller.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogBaseWeapon, All, All)
 
@@ -16,10 +20,53 @@ ASTUBaseWeapon::ASTUBaseWeapon()
 void ASTUBaseWeapon::BeginPlay()
 {
 	Super::BeginPlay();
+	check(WeaponMesh);
 }
+
 
 void ASTUBaseWeapon::Fire()
 {
 	UE_LOG(LogBaseWeapon, Display, TEXT("Fire!"));
+
+	MakeShot();
 }
+
+void ASTUBaseWeapon::MakeShot()
+{
+	if (!GetWorld()) return;
+
+	const auto Player = Cast<ACharacter>(GetOwner());
+	if (!Player) return;
+
+	const auto Controller = Player->GetController<APlayerController>();
+	if (!Controller) return;
+
+
+	FVector ViewLocation;
+	FRotator ViewRotation;
+	Controller->GetPlayerViewPoint(ViewLocation, ViewRotation);
+
+	const FTransform SocketTransform = WeaponMesh->GetSocketTransform(MuzzleSocketName);
+	const FVector TraceStart = ViewLocation;
+	const FVector ShootDirection = ViewRotation.Vector();
+	const FVector TraceEnd = TraceStart + ShootDirection * TraceMaxDistance;
+	
+	FCollisionQueryParams CollisionParams;
+	CollisionParams.AddIgnoredActor(GetOwner());
+
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECollisionChannel::ECC_Visibility, CollisionParams);
+
+	if (HitResult.bBlockingHit)
+	{
+		DrawDebugLine(GetWorld(), SocketTransform.GetLocation(), HitResult.ImpactPoint, FColor::Red, false, 3.0f, 0, 3.0f);
+		DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 24, FColor::Red, false, 5.0f);
+
+		UE_LOG(LogBaseWeapon, Display, TEXT("Bone: %s!"), *HitResult.BoneName.ToString());
+	}
+	else {
+		DrawDebugLine(GetWorld(), SocketTransform.GetLocation(), TraceEnd, FColor::Red, false, 3.0f, 0, 3.0f);
+	}
+}
+
 
